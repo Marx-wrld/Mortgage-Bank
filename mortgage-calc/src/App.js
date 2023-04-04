fa4b3da151596677fa122b2e00853358201b5734
Marx-wrld <iongonga@kabarak.ac.ke>
2023-04-04 12:58:41 +0300

    Navbar and importing the necessary components

--- mortgage-calc/src/App.js
+++ mortgage-calc/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
+  //Importing our Navbar component and rendering it to our app
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
 }
--- mortgage-calc/src/index.js
+++ mortgage-calc/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from "@mui/materials/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from  "./theme";
 import ReactDOM from 'react-dom/client';
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( //importing the theme file in our index.js file and wrapping the app with the ThemeProvider
+root.render( //importing the theme.js file in our index.js file and wrapping the app with the ThemeProvider
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <App />
