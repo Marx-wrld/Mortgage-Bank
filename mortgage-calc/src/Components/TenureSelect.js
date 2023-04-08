ce8e4bd805568d13c12e96df5856bc50c3aa8012
Marx-wrld <iongonga@kabarak.ac.ke>
2023-04-08 15:37:00 +0300

    Creating the TenureSelect component

--- mortgage-calc/src/Components/TenureSelect.js
+++ mortgage-calc/src/Components/TenureSelect.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react'; //importing required components from MUI library
+import { InputLabel } from '@mui/material';
+import MenuItem from '@mui/material';
+import FormControl from '@mui/material';
+import Select from '@mui/material';
 
-const TenureSelect = () => {
-  return (
-    <div>TenureSelect</div>
-  )
-}
+
+const TenureSelect = ({data, setData}) => {
+  const HandleChange = (event) => { //HandleChange function that will be used to set the tenure state
+    setData({...data, loanTerm: event.target.value});
+  }
+  return ()
+};
 
 export default TenureSelect
